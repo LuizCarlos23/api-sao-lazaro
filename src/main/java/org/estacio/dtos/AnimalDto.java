0529b5187960d8176be3c7e833353b87d27ec5a5
LuizCarlos23 <lc.estevao23@gmail.com>
2024-03-20 16:36:04 -0300

    add list and register animals

---
+++ src/main/java/org/estacio/dtos/AnimalDto.java
@@ -0,0 +1,17 @@
+package org.estacio.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnimalDto {
+    private Date entranceDate;
+    private String race;
+    private String local;
+    private String anamnesis;
+}
---
+++ src/main/java/org/estacio/entities/Animal.java
@@ -0,0 +1,30 @@
+package org.estacio.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "animals")
+public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "entrance_date")
+    private Date entranceDate;
+    private String race;
+    private String local;
+    private String anamnesis;
+
+    public Animal(Date entranceDate, String race, String local, String anamnesis) {
+        this.entranceDate = entranceDate;
+        this.race = race;
+        this.local = local;
+        this.anamnesis = anamnesis;
+    }
+}
